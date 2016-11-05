37f4985b83ea649778d22909e72d13046d3fe70a
AldrichMascarenhas <mascarenhasaldrich@gmail.com>
2016-11-05 21:22:30 +0530

    Fixes

--- src/services/user-check.service.ts
+++ src/services/user-check.service.ts
@@ -8,10 +8,12 @@ export class UserCheckService {
   UserID: string;
 
   ref: any;
-
+  auth: any;
 
   constructor(private _authService: AuthService, @Inject(FirebaseApp) firebaseApp: any) {
     this.ref = firebaseApp.database().ref(); //THis gets a reference to the base Database.
+    this.auth = firebaseApp.auth();
+
     this.UserID = _authService.getid();
 
 
@@ -22,28 +24,49 @@ export class UserCheckService {
 
     var restaurantRef = this.ref.child('restaurants');
     var users_restaurantRef = this.ref.child('users_restaurant');
+    var user = this.auth.currentUser.uid;
+
+    console.log(user);
 
 
-    users_restaurantRef.child(this.UserID).once('value').then(function (snapshot) {
+    users_restaurantRef.child(user).once('value').then(function (snapshot) {
 
       var exists = (snapshot.val() !== null);
 
-      userExistsCallback(exists);
+      userAssociatedWithRestaurant(exists);
 
     });
 
-    function userExistsCallback(exists) {
+    function userAssociatedWithRestaurant(exists) {
       if (exists) {
+        //If exists then get the key here and populate everything else
         alert('user exists!');
       } else {
-        alert('user does not exist!');
+        //Create that new Restaurant for the User.
+        createRestaurant();
       }
     }
 
+    function createRestaurant() {
+      var newPostRef = restaurantRef.push();
+      var restaurant_key = newPostRef.key;
+      newPostRef.set({
+        user: user
+      });
+
+      linkUserAndRestaurant(restaurant_key);
+    }
+
+    function linkUserAndRestaurant(key) {
+      var newRef = users_restaurantRef.child(user);
+      newRef.set({
+        restaurant: key
+      });
+
+    }
 
 
   }
-
 
 
   /* CALLNIG THIS DIRETLY WORKS WTF
