4e3370522de0daf2adf5c4dc25c58a48db065d67
AldrichMascarenhas <mascarenhasaldrich@gmail.com>
2016-11-06 22:52:24 +0530

    Made Change to MEnu Component

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, AfterViewInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements AfterViewInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private _titleService: Title,
+              private _router: Router,) {
+
   }
 
+  goBack(route: string): void {
+    this._router.navigate(['/']);
+  }
+
+  ngAfterViewInit(): void {
+    this._titleService.setTitle('Covalent Users');
+
+  }
 }
